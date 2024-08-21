1f6c80bb6a5885a8dc5bd5bf34e9623594641396
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-08-21 23:54:57 +0200

    Adding real data

--- app/courses/[slug]/page.jsx
+++ app/courses/[slug]/page.jsx
@@ -1,6 +1,6 @@
-import CourseBanner from '../../../components/sections/courses/coursedetails/CourseBanner'
-import CourseDetail from '../../../components/sections/courses/coursedetails/CourseDetails'
-import coursesFakes from '../../../utils/coursesFakes';
+import CourseBanner from '@/components/sections/courses/coursedetails/CourseBanner'
+import CourseDetail from '@/components/sections/courses/coursedetails/CourseDetails'
+import coursesFakes from '@/utils/coursesFakes';
 
 const page = ({ params }) => {
   const { slug } = params;
--- utils/coursesFakes.js
+++ utils/coursesFakes.js
@@ -1,14 +1,15 @@
 const CoursesData = [
     {
         id: "1",
-        title: "Hybrid Full-stack Course for Starters",
-        slug: "hybrid-full-stack-development",
+        title: "Beginner Software Development Trainings",
+        slug: "beginner-software-development-trainings",
+        category: "web-development",
         subTitle: "Create a good fundation in software development through project based learning", 
         description: "Kick of and upscale your web development skills throught real world projects with the most popular JavaScript frameworks",
         detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
         duration: "3 months",
         startDate: "2024-10-18T17:24:04.585Z",
-        price: 100,
+        price: 100000,
         coverImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         secondImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         isOpen: true,
@@ -49,14 +50,15 @@ const CoursesData = [
     },
     {
         id: "2",
-        title: "Data Science",
-        slug: "data-science",
-        subTitle: "Learn the basics of web development through projects using the most used JavaScript frameworks",
-        description: "A New Full-stack Software Development Course by SheCanCODE Bootcamp is open for applicants",
+        title: "Cloud Computing",
+        slug: "cloud-computing",
+        category: "cloud-computing",
+        subTitle: "Learn the basics of cloud computing and prepare yourself to work for the most famous cloud computing certifications",
+        description: "Get all the foundational knowledge to start working for AWS Certifications.",
         detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
-        duration: "4 months",
+        duration: "6 months",
         startDate: "",
-        price: 0,
+        price: 800000,
         coverImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         secondImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         isOpen: false,
@@ -99,12 +101,13 @@ const CoursesData = [
         id: "3",
         title: "Mobile Application Development",
         slug: "mobile-application-development",
+        category: "mobile-application-development",
         subTitle: "Learn the basics of web development through projects using the most used JavaScript frameworks",
         description: "A New Full-stack Software Development Course by SheCanCODE Bootcamp is open for applicants",
         detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
-        duration: "5 months",
+        duration: "6 months",
         startDate: "2025-01-20T17:24:04.585Z",
-        price: 200,
+        price: 150000,
         coverImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         secondImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         isOpen: false,
@@ -130,14 +133,15 @@ const CoursesData = [
     },
     {
         id: "4",
-        title: "Game Development",
-        slug: "game-development",
+        title: "UI/UX Design and Graphic Design Foundamentals",
+        slug: "ui/ux-design-and-graphic-design-foundamentals",
+        category: "design",
         subTitle: "Learn the basics of web development through projects using the most used JavaScript frameworks",
         description: "A New Full-stack Software Development Course by SheCanCODE Bootcamp is open for applicants",
         detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
-        duration: "6 months",
+        duration: "3 months",
         startDate: "",
-        price: 250,
+        price: 100000,
         coverImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         secondImage: "https://shecancode-website-dev.vercel.app/education-2.png",
         isOpen: false,
@@ -178,8 +182,9 @@ const CoursesData = [
     },
     {
         id: "5",
-        title: "UX/UI Design",
-        slug: "ux-ui-design",
+        title: "Advanced UX/UI Design",
+        slug: "advanced-ux-ui-design",
+        category: "design",
         subTitle: "Learn the basics of web development through projects using the most used JavaScript frameworks",
         description: "A New Full-stack Software Development Course by SheCanCODE Bootcamp is open for applicants",
         detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
@@ -201,11 +206,26 @@ const CoursesData = [
 ];
 
 const CourseCategories = [
-    "Web Development",
-    "Data Science",
-    "Mobile Development",
-    "Game Development",
-    "UX/UI Design"
+    {
+        name: "All",
+        slug: "all",
+    },
+    {
+        name: "Web Development",
+        slug: "web-development",
+    },
+    {
+        name: "Cloud computing",
+        slug: "cloud-computing",
+    },
+    {
+        name: "Mobile Application Development",
+        slug: "mobile-application-development",
+    },
+    {
+        name: "Design",
+        slug: "design",
+    }
 ];
 
 const data = {
