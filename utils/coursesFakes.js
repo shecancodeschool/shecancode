be921384ee3d68715b6ac2c4740ce9455134fee9
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2024-08-23 12:37:37 +0200

    Updates

--- utils/coursesFakes.js
+++ utils/coursesFakes.js
@@ -4,98 +4,120 @@ const CoursesData = [
         title: "Beginner Software Development Trainings",
         slug: "beginner-software-development-trainings",
         category: "web-development",
-        subTitle: "Create a good fundation in software development through project based learning", 
+        subTitle: "Create a good fundation in software development through project based learning",
         description: "Kick of and upscale your web development skills throught real world projects with the most popular JavaScript frameworks",
-        detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
+        detailedDescription: {
+            paragraphOne: "In this course, you will get a solid fundation in software development throught project based learning. You will start by understanding the key principles of Visual Design, the learn designing user interfaces using Figma, next you will turn into code the user interfaces designed in figma using HTML and CSS. Finally, you will learn how to build interactive user interfaces by applying JavaScript to your static web pages.",
+            paragraphTwo: "You will spend alot of time learning and mastering JavaScript as the key programming language that will strengthern you knowledge and experience that will enable you to easily learn building applications using any frontend and mobile application framework.",
+            paragraphThree: "",
+            paragraphFour: "",
+        },
         duration: 12,
         durationType: "Weeks",
-        startDate: "2024-10-18T17:24:04.585Z",
+        startDate: "2024-10-07T17:24:04.585Z",
         price: 100000,
         coverImage: "/F5.jpg",
         secondImage: "/F5.jpg",
         isOpen: true,
         keyInfo: [
-            { title: "Authentic Certificate", desc: "Earn a Certificate upon completion" },
-            { title: "On-site Classes", desc: "Start instantly and learn at your own pace" },
-            { title: "Live Frameworks", desc: "Set and maintain flexible deadlines." },
-            { title: "Beginner Level", desc: "No prior experience required." },
-            { title: "Rigorous Curriculum", desc: "English" }
+            { title: "Level", desc: "Beginner" },
+            { title: "Start Date", desc: "October 7, 2024" },
+            { title: "Duration & Location", desc: "12 weeks, Hybrid" },
+            { title: "Price", desc: "100,000 Rwf" },
+            { title: "Schedule", desc: "5 Days/Week, (3 days onsite, 2 days online)" },
         ],
-        faqs: [
+        prerequisites: [
+            "Have a working laptop that meets the following baseline specifications: Core i5 and upwards, 4GB RAM, and at least 256GB of storage.",
+            "Be capable of dedicating a minimum of 6 hours a day to the program.",
+            "Having access to internet access when you are not working from the campus.",
+            "You must not be enrolled in other training programs to maximize your success rate."
+        ],
+        modules: [
             {
                 id: 1,
-                question: "Do I need to take both the UX and UI programs to learn about both disciplines?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "Fundamentals of User Interface and Graphic Design",
+                answer: "Using Figma and other tools, You will learn design principles used to build the most effective user interfaces, posters, and websites. You will understand how to apply typography, space, colors, and more when designing an effective user interface."
             },
             {
                 id: 2,
-                question: "Are there payment plans available?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "HTML, CSS and the Introduction to Web Design",
+                answer: "In this module, you will dive deeper into learning how to transform user interfaces you designed with Figma into actual code. You will use HTML to set markups of your content, and CSS to give styles. You will also learn how to design websites that look good in all devices. By the end of this module, you will be able to turn any given design into a visually appealing website."
             },
             {
                 id: 3,
-                question: "What’s the “catch” for the job guarantee?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "JavaScript Fundamentals",
+                answer: "In this module, it's when you will learn an actual programming language (JavaScript). You will start by learning the synthax, use case, flow control, and more. By the end of this module, you will be able to solve real life problems, by writing high quality programs that solve real life problems."
             },
             {
                 id: 4,
-                question: "What kind of job can I get after the program?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "DOM Manipulation",
+                answer: "In this module, you will learn how to use JavaScript to make the user interfaces designed in the second module interactive. You will work on a number of challenges such as building Todo Apps, Weather apps, and more. A this stage, you will be very comfortable enough to be able to build a fully responsive and interactive website of any kind. A the end of this module, you will put into practice everything you learned in the first 3 modules and create your own portfolio."
             },
             {
                 id: 5,
-                question: "What if I have a question that isn’t answered here?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "Capstone projects and customer products",
+                answer: "This module while being the last, it is the most important of all. In this module, you will cement your knowledge through working on timed projects of different variations, you will apply animations and use your creativity to approach design challenges."
             },
         ]
     },
     {
         id: "2",
-        title: "Cloud Computing",
-        slug: "cloud-computing",
-        category: "cloud-computing",
-        subTitle: "Learn the basics of cloud computing and prepare yourself to work for the most famous cloud computing certifications",
-        description: "Get all the foundational knowledge to start working for AWS Certifications.",
-        detailedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet sem. Etiam fringilla augue quam, eget ullamcorper elit porta ac. Aliquam et metus nec neque consequat pulvinar. Aliquam cursus magna vitae mi sollicitudin placerat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent sit amet metus neque. Pellentesque quis nunc sit amet lorem facilisis consequat in et orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et accumsan lacus, id aliquam ligula. Aliquam venenatis malesuada magna, sit amet bibendum eros luctus auctor. Vestibulum ut molestie justo. Aliquam erat volutpat. Duis enim odio, vulputate nec blandit eget, pulvinar a urna.",
-        duration: 6,
-        durationType: "Months",
-        startDate: "",
-        price: 800000,
-        coverImage: "/F2.jpg",
-        secondImage: "/F2.jpg",
-        isOpen: false,
+        title: "Intermediate level web development training",
+        slug: "intermediate-level-web-development-training",
+        category: "web-development",
+        subTitle: "Use ReactJS, NextJS and ExpressJS to easily build full-stack web-applications",
+        description: "Build highly effiscient, easy to integrate and fully functional web-based applications by leveraging the benefits of ReactJS 18+, NextJS 14 and Express.",
+        detailedDescription: {
+            paragraphOne: "In this course, you will learn how to use web development frameworks and libraries such as ReactJS, NextJS, and ExpressJS to quickly build, deploy and maintain high quality applications in a short period. You will learn how to store, retrieve, and manage data by connecting your projects to databases such as MongoDB and PostgreSQL.",
+            paragraphTwo: "You will spend alot of time learning and mastering JavaScript as the key programming language that will strengthern you knowledge and experience that will enable you to easily learn building applications using any frontend and mobile application framework.",
+            paragraphThree: "",
+            paragraphFour: "",
+        },
+        duration: 12,
+        durationType: "Weeks",
+        startDate: "2025-01-06T17:24:04.585Z",
+        price: 100000,
+        coverImage: "/F5.jpg",
+        secondImage: "/F5.jpg",
+        isOpen: true,
         keyInfo: [
-            { title: "Authentic Certificate", desc: "Earn a Certificate upon completion" },
-            { title: "On-site Classes", desc: "Start instantly and learn at your own pace" },
-            { title: "Live Frameworks", desc: "Set and maintain flexible deadlines." },
-            { title: "Beginner Level", desc: "No prior experience required." },
-            { title: "Rigorous Curriculum", desc: "English" }
+            { title: "Level", desc: "Intermediate" },
+            { title: "Start Date", desc: "January 6, 2025" },
+            { title: "Duration & Location", desc: "12 weeks, Hybrid" },
+            { title: "Price", desc: "100,000 Rwf" },
+            { title: "Schedule", desc: "5 Days/Week, (3 days onsite, 2 days online)" },
         ],
-        faqs: [
+        prerequisites: [
+            "Have a working laptop that meets the following baseline specifications: Core i5 and upwards, 4GB RAM, and at least 256GB of storage.",
+            "Be capable of dedicating a minimum of 6 hours a day to the program.",
+            "Having access to internet access when you are not working from the campus.",
+            "You must not be enrolled in other training programs to maximize your success rate."
+        ],
+        modules: [
             {
                 id: 1,
-                question: "Do I need to take both the UX and UI programs to learn about both disciplines?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "Fundamentals of User Interface and Graphic Design",
+                answer: "Using Figma and other tools, You will learn design principles used to build the most effective user interfaces, posters, and websites. You will understand how to apply typography, space, colors, and more when designing an effective user interface."
             },
             {
                 id: 2,
-                question: "Are there payment plans available?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "HTML, CSS and the Introduction to Web Design",
+                answer: "In this module, you will dive deeper into learning how to transform user interfaces you designed with Figma into actual code. You will use HTML to set markups of your content, and CSS to give styles. You will also learn how to design websites that look good in all devices. By the end of this module, you will be able to turn any given design into a visually appealing website."
             },
             {
                 id: 3,
-                question: "What’s the “catch” for the job guarantee?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "JavaScript Fundamentals",
+                answer: "In this module, it's when you will learn an actual programming language (JavaScript). You will start by learning the synthax, use case, flow control, and more. By the end of this module, you will be able to solve real life problems, by writing high quality programs that solve real life problems."
             },
             {
                 id: 4,
-                question: "What kind of job can I get after the program?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "DOM Manipulation",
+                answer: "In this module, you will learn how to use JavaScript to make the user interfaces designed in the second module interactive. You will work on a number of challenges such as building Todo Apps, Weather apps, and more. A this stage, you will be very comfortable enough to be able to build a fully responsive and interactive website of any kind. A the end of this module, you will put into practice everything you learned in the first 3 modules and create your own portfolio."
             },
             {
                 id: 5,
-                question: "What if I have a question that isn’t answered here?",
-                answer: "Yes, you can select a monthly payment plan of $550.00 a month for 10 Months and $1,400.00 upfront. Note: You will be required to pay the full duration of the payment plan even if you finish the program before the payment plan ends. Still doesn’t work for you? Book a call with a Career Advisor and see if you are eligible for a custom payment plan."
+                question: "Capstone projects and customer products",
+                answer: "This module while being the last, it is the most important of all. In this module, you will cement your knowledge through working on timed projects of different variations, you will apply animations and use your creativity to approach design challenges."
             },
         ]
     },
@@ -234,7 +256,7 @@ const CourseCategories = [
 ];
 
 const data = {
-    CoursesData, 
+    CoursesData,
     CourseCategories
 };
 
