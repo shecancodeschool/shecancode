b1d696fa95c7f83181e42fbe6d730800a6b8cbc0
SheCanCODE School <155643931+shecancodeschool@users.noreply.github.com>
2024-08-28 11:45:29 +0200

    Delete components directory

--- components/ReusablePageStructure.jsx
+++
@@ -1,9 +0,0 @@
-const ReusablePageStructure = ({children}) => {
-    return (
-        <div className="flex flex-col items-start justify-start w-full">
-            {children}
-        </div>
-    )
-}
-
-export default ReusablePageStructure
