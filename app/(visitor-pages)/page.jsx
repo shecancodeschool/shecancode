21daa953fc82d74961af371a9c8609a062d7f97f
SheCanCODE School <155643931+shecancodeschool@users.noreply.github.com>
2024-08-28 12:28:19 +0200

    Add files via upload

--- app/(visitor-pages)/page.jsx
+++ app/(visitor-pages)/page.jsx
@@ -1,20 +1,16 @@
-// import { getHomePageCourses } from "./_actions/courses"
 import ArticlesSection from "./_components/sections/home/ArticlesSection";
 import CoursesSection from "./_components/sections/home/CoursesSection";
 import FaqSection from "./_components/sections/home/faq/FaqSection";
-import HomeBanner from "./_components/sections/home/HomeBanner";
 import HomeSloganSection from "./_components/sections/home/HomeSloganSection";
-import PartnersSection from "./_components/sections/home/PartnersSection";
-import ReviewsSection from "./_components/sections/home/review/ReviewsSection";
 import ServicesSection from "./_components/sections/home/ServicesSection";
-import StatisticsSection from "./_components/sections/home/StatisticsSection";
+import PartnersSection from "./_components/sections/home/PartnersSection";
 
 import HomePageData from "/utils/homePageFakes";
 import ArticlesFakes from "/utils/blogsAndCategoriesFakes";
-import PopupModel from "./_components/sections/home/popup/PopupModel";
 import { getHomePageCourses } from "./_actions/courses";
-import Ad from "./_components/sections/home/Ad";
 import HomeBannerTwo from "./_components/sections/home/HomeBannerTwo";
+import ReviewsSection from "./_components/sections/home/review/ReviewsSection";
+import StatisticsSection from "./_components/sections/home/StatisticsSection";
 
 const jsonLd = {
   '@context': 'https://shecancode.vercel.app',
@@ -32,21 +28,17 @@ const page = async () => {
     console.log(error);
   }
 
-  const modelCourse = courses.length > 0 ? courses[0] : null;
-
   return (
     <div className="relative">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-      {/* {modelCourse && <PopupModel course={modelCourse} />} */}
       <HomeBannerTwo
         bannerData={HomePageData.bannerData}
         statistics={HomePageData.statistics}
         course={courses[0]}
       />
-      {/* {modelCourse && <Ad course={modelCourse} />} */}
       <HomeSloganSection
         sloganDescriptionData={HomePageData.sloganDescriptionData}
       />
