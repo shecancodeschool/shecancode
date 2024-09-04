364e445eb4fe0aafa852fc0d4c4eab38b3408261
SheCanCODE School <155643931+shecancodeschool@users.noreply.github.com>
2024-09-04 11:07:47 +0200

    Update firebase.js

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -1,21 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-const firebaseApiKey = "";
+const firebaseApiKey = process.env.FIREBASE_API_KEY;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: firebaseApiKey,
-  authDomain: "trash-mark.firebaseapp.com",
-  projectId: "trash-mark",
-  storageBucket: "trash-mark.appspot.com",
-  messagingSenderId: "883773779524",
-  appId: "1:883773779524:web:45af058d5971e3ed5b1611"
+  apiKey: "AIzaSyAjL4aDtWhHacDU6VoYFKxEJGoX5bclLRg",
+  authDomain: "shecancode-website.firebaseapp.com",
+  projectId: "shecancode-website",
+  storageBucket: "shecancode-website.appspot.com",
+  messagingSenderId: "483656872258",
+  appId: "1:483656872258:web:b8699c8098a79ce4c898db",
+  measurementId: "G-4TYBXD0DK3"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
