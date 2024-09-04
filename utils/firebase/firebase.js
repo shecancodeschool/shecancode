42d55e610ec62795f25e66f5786cf270183eedf7
SheCanCODE School <155643931+shecancodeschool@users.noreply.github.com>
2024-09-04 14:51:55 +0200

    Update firebase.js

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,4 +22,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
