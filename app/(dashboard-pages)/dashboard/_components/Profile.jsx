501c61a44dd45392581c44bbf79300864d7d99f1
SheCanCODE School <155643931+shecancodeschool@users.noreply.github.com>
2024-09-16 18:53:53 +0200

    Add files via upload

--- app/(dashboard-pages)/dashboard/_components/Profile.jsx
+++ app/(dashboard-pages)/dashboard/_components/Profile.jsx
@@ -4,18 +4,25 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { signOut, useSession } from "next-auth/react"
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import { Suspense } from "react";
+import Loading from "../loading";
 
 export default function Profile() {
     const { data: session, status } = useSession();
     return (
-        <div className="flex-center">
-            <div className="w-[450px] space-y-4">
-                <UserAvatar />
-                <Input type="text" placeholder="Name" value={session?.user?.name} className="text-color-text" />
-                <Input type="email" placeholder="Email" value={session?.user?.email} className="text-color-text" />
-                <Button variant="destructive" className="w-fit" onClick={() => signOut({ callbackUrl: "/auth/login" })}>Logout</Button>
-            </div>
-        </div>
+        <ErrorBoundary>
+            <Suspense fallback={<Loading />}>
+                <div className="flex-center">
+                    <div className="w-[450px] space-y-4">
+                        <UserAvatar />
+                        <Input type="text" placeholder="Name" value={session?.user?.name} className="text-color-text" />
+                        <Input type="email" placeholder="Email" value={session?.user?.email} className="text-color-text" />
+                        <Button variant="destructive" className="w-fit" onClick={() => signOut({ callbackUrl: "/auth/login" })}>Logout</Button>
+                    </div>
+                </div>
+            </Suspense>
+        </ErrorBoundary>
     )
 }
 
--- app/(dashboard-pages)/dashboard/loading.js
+++ app/(dashboard-pages)/dashboard/loading.js
@@ -1,4 +1,6 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <p>Loading...</p>
+    return <Loader2 size={30} className="mx-auto my-10 animate-spin" />
   }
