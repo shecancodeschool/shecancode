9de761f5f1a0ed00fac2010f414018835b6aff9f
SheCanCODE School <155643931+shecancodeschool@users.noreply.github.com>
2024-09-16 18:50:33 +0200

    Add files via upload

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -13,7 +13,18 @@ const sendEmail = (recipient, subject, body) => {
         from: `"SheCanCODE Bootcamp" <${process.env.EMAIL_ADDRESS}>`,
         to: recipient,
         subject: subject,
-        text: body
+        text: body,
+        replyTo: "education@igirerwanda.org",
+        // html: `
+        //     <div style="color: #333; background-color: #eee; padding: 20px; border-radius: 5px;">
+        //     <h1>${subject}</h1>
+        //     <p>${body}</p>
+        //     <br />
+        //     <p>
+        //         Reply to: <a href="mailto:education@igirerwanda.org">education@igirerwanda.org</a>
+        //     </p>
+        //     </div>
+        // `
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
