d8255fc5ed69ac35372b9c5c98632d84020fd6fc
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-10-28 19:21:59 +0200

    Update: Job Details Metadata

--- app/(visitor-pages)/careers/[id]/page.jsx
+++ app/(visitor-pages)/careers/[id]/page.jsx
@@ -2,17 +2,46 @@ import JobDescriptionPageBanner from '../../_components/sections/careers/JobDesc
 import JobDescriptionContainter from '../../_components/sections/careers/JobDescriptionContainter';
 import { getArticleById } from '@/app/(dashboard-pages)/dashboard/_actions/articlesActions';
 
+var metadata = {};
+
 const page = async ({ params }) => {
+  
+
   const { id } = params;
   const response = await getArticleById(id);
   const job = JSON.parse(response);
-  
+
+  metadata = {
+    title: `${job.title}`,
+    description: `${job.description}`,
+    keywords: "SheCanCODE, Career, Programs at SheCanCODE, SheCanCode Courses, Bootcamp, Women in tech, Training bootcamp, IT, IT Bootcamp, Free bootcamp, Girls, Girls bootcamp in Rwanda, Igire Rwanda Organization",
+    openGraph: {
+      title: `${job.title}`,
+      description: `${job.description}`,
+      ...{ images: [job.image] },
+    },
+  };
+
+  const jsonLd = {
+    '@context': `https://www.shecancodeschool.org/careers/${job.id}`,
+    '@type': 'Job',
+    name: `${job.title}`,
+    image: `${job.image}`,
+    description: `${job.description}`,
+  }
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <JobDescriptionPageBanner job={job} />
       <JobDescriptionContainter job={job} />
     </>
   )
 }
+
+export var metadata = metadata;
 
 export default page;
