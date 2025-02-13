8b7b78c229e5ddfb4ce96651b176153197cb9dd4
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-02-13 17:55:18 +0200

    Delete app/(visitor-pages) directory

--- app/(visitor-pages)/_components/SubmitButton.jsx
+++
@@ -1,8 +0,0 @@
-function SubmitButton() {
-    const { pending } = useFormStatus();
-    return (
-        <button type="submit" disabled={pending} className='px-6 py-2 w-fit rounded-sm bg-sky-950 hover:bg-sky-500 text-white font-bold'>
-            {pending ? "Submitting..." : "Apply"}
-        </button>
-    )
-}
