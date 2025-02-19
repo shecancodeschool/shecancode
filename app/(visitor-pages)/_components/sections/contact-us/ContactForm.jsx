6b3be53cdb047a1789ce53cbb1f53a45fb797d5c
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-02-19 10:55:18 +0200

    Update ContactForm.jsx

--- app/(visitor-pages)/_components/sections/contact-us/ContactForm.jsx
+++ app/(visitor-pages)/_components/sections/contact-us/ContactForm.jsx
@@ -1,102 +1,92 @@
-"use client";
+  "use client";
 
-import ReusableSection from '../../ReusableSection';
-import { toast } from 'sonner';
-import { useFormState, useFormStatus } from'react-dom';
-import { contactUs } from '@/app/(visitor-pages)/_actions/contactus';
-import { SubmitButton } from '../footer/SubscribeForm';
-export default function ContactForm() {
-  const [response, action] = useFormState(contactUs, {});
-  if (response.error) {
-    toast.error(response.error);
-  } else if (response.message) {
-    toast.success(response.message);
-    setTimeout(() => {
-      window.location.reload();
-    }, 2000);
-  }
+  import ReusableSection from '../../ReusableSection';
+  import { toast } from 'sonner';
+  import { useFormState, useFormStatus } from'react-dom';
+  import { contactUs } from '@/app/(visitor-pages)/_actions/contactus';
+  import { SubmitButton } from '../footer/SubscribeForm';
+  export default function ContactForm() {
+    const [response, action] = useFormState(contactUs, {});
+    if (response.error) {
+      toast.error(response.error);
+    } else if (response.message) {
+      toast.success(response.message);
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    }
 
-  return (
-    <ReusableSection isTopSection background="">
-      <div className='flex justify-between flex-wrap bg-white w-full border p-8 md:p-20 shadow-lg rounded-md'>
-        {/* Left side  */}
-        <div className="flex flex-col justify-between items-start w-full md:w-[48%] mb-10">
-          <form className="space-y-6 w-full text-black" action={action}>
-            <div className="flex gap-4 mb-4 flex-col">
-              <label htmlFor='name' className='flex flex-col space-y-2'>
-                <input
-                  type='text'
-                  name='name'
-                  id='name'
-                  className='border border-gray-300 rounded-md p-2 w-full bg-slate-100'
-                  placeholder="Full Name"
-                  required
-                />
-              </label>
-              <label htmlFor='email' className='flex flex-col space-y-2'>
-                <input
-                  type='email'
-                  name='email'
-                  id='email'
-                  className='border border-gray-300 rounded-md p-2 w-full bg-slate-100'
-                  placeholder="Email address"
-                  required
-                />
-              </label>
-              <label htmlFor='message' className='flex flex-col space-y-2'>
-                <textarea
-                  name='message'
-                  id='message'
-                  className='border bg-slate-50 border-gray-300 rounded-md p-2 w-full h-100'
-                  placeholder="Your message here..."
-                  required
-                />
-              </label>
-              <SubmitButton label={'SEND MESSAGE'}/>
+    return (
+      <ReusableSection isTopSection background="">
+        <div className='flex justify-between flex-wrap bg-white w-full border p-8 md:p-20 shadow-lg rounded-md'>
+          {/* Left side  */}
+          <div className="flex flex-col justify-between items-start w-full md:w-[48%] mb-10">
+            <form className="space-y-6 w-full text-black" action={action}>
+              <div className="flex gap-4 mb-4 flex-col">
+                <label htmlFor='name' className='flex flex-col space-y-2'>
+                  <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    className='border border-gray-300 rounded-md p-2 w-full bg-slate-100'
+                    placeholder="Full Name"
+                    required
+                  />
+                </label>
+                <label htmlFor='email' className='flex flex-col space-y-2'>
+                  <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    className='border border-gray-300 rounded-md p-2 w-full bg-slate-100'
+                    placeholder="Email address"
+                    required
+                  />
+                </label>
+                <label htmlFor='message' className='flex flex-col space-y-2'>
+                  <textarea
+                    name='message'
+                    id='message'
+                    className='border bg-slate-50 border-gray-300 rounded-md p-2 w-full h-100'
+                    placeholder="Your message here..."
+                    required
+                  />
+                </label>
+                <SubmitButton label={'SEND MESSAGE'}/>
+              </div>
+            </form>
+          </div>
+
+          {/* Right side  */}
+          <div className="space-y-4 flex flex-col justify-between items-start w-full md:w-[48%]">
+            <div className="text-black font text-base md:text-lg">
+              <p>SheCanCODE BootCamp</p>
+              <p>KG 549 St, 36 Kacyiru</p>
+              <p>
+                <a
+                  href="https://www.google.com/maps?q=Impact+Center,+Kacyiru"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline"
+                >
+                  Impact Center, Kacyiru
+                </a>
+              </p>
+              <p>+250 788 473 533</p>
+              <p>education@shecancodeschool.org</p>
             </div>
-          </form>
-        </div>
-
-        {/* Right side  */}
-        <div className="space-y-4 flex flex-col justify-between items-start w-full md:w-[48%]">
-          <div className="text-black font text-base md:text-lg">
-            <p>SheCanCode BootCamp</p>
-            <p>KG 549 Street</p>
-            <p>
-              <a
-                href="https://www.google.com/maps?q=Impact+Center,+Kacyiru"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                Impact Center, Kacyiru
-              </a>
-            </p>
-            <p>+250 788 473 533</p>
-            <p>education@igirerwanda.org</p>
-          </div>
-          <div className="w-full flex justify-start items-cente md:w-full h-40 overflow-hidden">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.553554881119!2d30.072044274050132!3d-1.9306041366678404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca70041075f0d%3A0x8dd8bd686d407d92!2sSheCanCode%20Training%20Center!5e0!3m2!1sfr!2srw!4v1717664442768!5m2!1sfr!2srw"
-              width="500"
-              height="190"
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+            <div className="w-full flex justify-start items-cente md:w-full h-40 overflow-hidden">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.553554881119!2d30.072044274050132!3d-1.9306041366678404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca70041075f0d%3A0x8dd8bd686d407d92!2sSheCanCode%20Training%20Center!5e0!3m2!1sfr!2srw!4v1717664442768!5m2!1sfr!2srw"
+                width="500"
+                height="190"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
-      </div>
-    </ReusableSection>
-  );
-};
-
-
-// function SubmitButton() {
-//   const { pending } = useFormStatus();
-//   return (
-//     <button type="submit" disabled={pending} className='px-3 py-2 rounded-sm bg-sky-950 hover:bg-sky-500 text-white font-bold w-full'>
-//       {pending ? "Submitting..." : "SUBSCRIBE"}
-//     </button>
-//   )
-// }
+      </ReusableSection>
+    );
+  };
